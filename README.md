1d3202ddf365589946bf62a2c9cd239460479684
AkeemOB <sonion13@gmail.com>
2024-04-05 11:08:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Xiazen_engine
+ Just messing with OpenGL and trying to recreate some games using OpenGL and maybe adding a side thing of the OLConsoleEngine as well.
---
+++ Rebuilding Concepts.cpp
@@ -0,0 +1,132 @@
+// Rebuilding Concepts.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+float screen_width = 1280;
+float screen_height = 720;
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    GLFWwindow* window = glfwCreateWindow(screen_width, screen_height, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    else {
+        std::cout << "Sucessfully Created GLFW window" << std::endl;
+    }
+
+
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //glViewport(0, 0, 800, 600);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    else {
+        std::cout << "Sucessfully initialize GLAD" << std::endl;
+    }
+
+    glViewport(0, 0, screen_width, screen_height);
+
+    // This is where I'll build my first triangle. Then object.
+
+    // 3 3D-vectors that outline a point.
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+
+    //vertex buffer objects stores large batches of vertices to the graphics card.
+    // This is an OpenGL Object. 
+    unsigned int VBO;
+
+    glGenBuffers(1, &VBO); // This object has an ID generated.
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // Sets the type of Objet.
+
+    /*
+    * Copys user-defined data into the currently bound buffer.
+    * First the Type of Object.
+    * Size of the data in Bytes
+    * The actually data being sent.
+    * How the GPU should manage this data. ->
+    *
+        GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+        GL_STATIC_DRAW: the data is set only once and used many times.
+        GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+
+    std::cout << "Init Program Loop" << std::endl;
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+        // rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap the buffers
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+
+/*! @brief This function is the callback that sets the framebuffer resize for the specified window.
+ * The function this works in as a callback is;
+ * 
+ * glfwSetFramebufferSizeCallback(GLFWwindow* window, GLFWframebuffersizefun callback); 
+ * 
+ *  @since Added in version ?-0.?-0.
+ *
+ *  @ingroup window
+ */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+// Maybe I can propergate an Input class throughout something, like Godot.
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
